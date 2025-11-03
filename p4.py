d43bfcbdb0f2d8a453399f6c628f938dfbd84178
Rana16468 <amsohelrana.me@gmail.com>
2025-11-03 11:35:03 +0600

    resolve problem 4

---
+++ p4.py
@@ -0,0 +1,16 @@
+# process 1 -->proble number 4
+a=list((10,20,30,40,50,60))
+for n in a:
+    result=str(n)
+    print(type(result).__name__,'the result', result)
+
+
+#process  2
+# b = [10, 20, 30, 40, 50, 60]
+# string_lists = [str(n) for n in b]
+# print(string_lists)
+
+#process 3
+# c=[10,20,30,40,50,60,70,80,90,100]
+# d=list(map(str, c))
+# print(d)
