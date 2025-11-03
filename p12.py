7b9941079659e2d5a7bd0c8b500b01eae5b47135
Rana16468 <amsohelrana.me@gmail.com>
2025-11-03 11:42:09 +0600

    resolving the p11 and p12

---
+++ p12.py
@@ -0,0 +1,6 @@
+#problem 12
+a=int(input("Enter The First Number: "))
+b=int(input("Enter The Second Number: "))
+c=int(input("Enter The Second Number :" ))
+result=max(a,b,c)
+print("the max number is ", result)
